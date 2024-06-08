17bdb0d9daf68f81d5bfb3af9cc7b9912728af87
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-06-08 13:03:59 +0400

    c

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
+
   login() {
     return { msg: 'I am signin' };
   }
