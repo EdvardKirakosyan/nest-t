448e86e0fb8d2d526e4b1ee0aefd4409040c8816
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-06-08 10:10:39 +0400

    c

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Post('signup')
   signup() {
